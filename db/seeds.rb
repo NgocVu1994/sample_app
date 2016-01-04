a98a75ba39aa40cf6cd491bb331a6b42b71a3e9b
Ngoc Vu <ngocvu.uet@gmail.com>
2016-01-04 08:37:47 +0700

    add title to micropost

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,9 @@ end
 
 users = User.order(:created_at).take(6)
 50.times do
+  title = "hello world"
   content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
+  users.each { |user| user.microposts.create!(content: content, title: title) }
 end
 
 # Following relationships
