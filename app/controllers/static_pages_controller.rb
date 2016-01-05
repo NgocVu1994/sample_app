ac8b07fefe60f25c0b6db2e344a3315f184ca49a
Ngoc Vu <ngocvu.uet@gmail.com>
2016-01-05 09:54:56 +0700

    lnslkfnlsnd

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     else
-    
+      @microposts = Micropost.all.paginate(page: params[:page])
     end
 
   end
